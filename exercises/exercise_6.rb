20388a9855288909c059bdbb1660fa32dc89c811
hrakad <h.alrakad82@hotmail.com>
2022-01-10 06:41:04 +0000

    Complete excersise 6 in Active Record -AR- assignment -Add relational data inside model classes & create mock employees-

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Adam", last_name: "Smith", hourly_rate: 45)
+@store1.employees.create(first_name: "Andrea", last_name: "Thomas", hourly_rate: 45)
+
+@store2.employees.create(first_name: "Lisa", last_name: "Tyler", hourly_rate: 65)
+@store2.employees.create(first_name: "Anna", last_name: "Sandrson", hourly_rate: 55)
+@store2.employees.create(first_name: "Ali", last_name: "Azzam", hourly_rate: 45)
