d9c8c9f434d66f781cd6046379d8be01140a564d
hrakad <h.alrakad82@hotmail.com>
2022-01-10 05:30:13 +0000

    Complete excersise 4 in Active Record -AR- assignment -Loading a subset of stores-

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: false, womens_apparel: true)
+
+puts Store.all.count
+
+@mens_stores = Store.where(mens_apparel: true)
+
+# puts @mens_stores
+
+@mens_stores.each do |mens_store|
+  puts "Location: #{mens_store.name}"
+end
+
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+
+@womens_stores.each do |womens_store|
+  puts "Location: #{womens_store.name}"
+end
